f575ad7ba768a504c3318709ee57d806830f7839
Justin Reese <justin.x.reese@gmail.com>
2013-10-24 12:24:25 -0400

    White space fixes

--- instaplaces.rb
+++ instaplaces.rb
@@ -12,7 +12,7 @@ Instagram.configure do |config|
   config.client_secret = ENV['CLIENT_SECRET']
 end
 
-class InstagramPost 
+class InstagramPost
   attr_accessor :link, :thumb_url, :location_name, :location_lat, :location_lng
 
   def to_html
@@ -38,7 +38,7 @@ class Instaplaces
   get '/stylesheets/style.css' do
     sass :style
   end
-  
+
   get '/camera.png' do
     send_file('public/camera.png')
   end
@@ -46,17 +46,17 @@ class Instaplaces
   get "/needs_instagram_auth" do
     haml '<a href="/oauth/connect">Connect with Instagram</a>'
   end
-  
+
   get "/oauth/connect" do
     redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
   end
-  
+
   get "/oauth/callback" do
     response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
     session[:access_token] = response.access_token
     redirect "/feed"
   end
-  
+
   get "/error/:intent" do
     intent = params[:intent].gsub(/[^a-z]/,'')
     if intent == 'timeout'
@@ -71,11 +71,11 @@ class Instaplaces
       {error:"Instagram service unavailable"}.to_json
     end
   end
-  
+
   get "/feed" do
     client = Instagram.client(:access_token => session[:access_token])
     user = client.user
-  
+
     html = "<h1>#{user.username}'s recent photos</h1>"
     for media_item in client.user_recent_media
       html << "<img src='#{media_item.images.thumbnail.url}'>"
@@ -119,16 +119,16 @@ class Instaplaces
 
   get "/nearby/:lat_lng" do
     client = Instagram.client(:access_token => session[:access_token])
-  
+
     # loc_string = "40.405784,-79.908714"
     loc_string = params[:lat_lng].gsub(/[^0-9,\.-]/,'')
     lat = loc_string.split(",")[0]
     lng = loc_string.split(",")[1]
     html = "<h3>Photos near <a href='/nearby/#{lat},#{lng}'>#{lat},#{lng}</a></h3>"
-  
+
     begin
       media_items = Timeout::timeout(15){
-        client.media_search(lat,lng,{:count =>100, :distance => 5000, 
+        client.media_search(lat,lng,{:count =>100, :distance => 5000,
           :max_timestamp => Time.now.to_i, :min_timestamp => (Date.today - (2*365)).to_time.to_i})
       }
     rescue Exception => e
@@ -142,14 +142,14 @@ class Instaplaces
     media_items.each do |media_item|
       places[media_item.location.id] = [] unless places[media_item.location.id]
       post = InstagramPost.new
-      post.link = media_item.link 
+      post.link = media_item.link
       post.thumb_url = media_item.images.thumbnail.url
-      post.location_name = media_item.location.name 
-      post.location_lat = media_item.location.latitude 
+      post.location_name = media_item.location.name
+      post.location_lat = media_item.location.latitude
       post.location_lng = media_item.location.longitude
       places[media_item.location.id] << post
     end # media_item
-  
+
     places.sort{|a, b| -1*(a[1].count <=> b[1].count)}.each do |place_id,posts|
       html << "<div class='place'>"
       if place_id
